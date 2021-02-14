84374bdb3b8dc44f0ef074a3d3966adfc09ab681
MattCCC <deindesign.pl@gmail.com>
2021-02-14 15:24:32 +0100

    Disable prettier for the package

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: '999.999.999',
+    },
+  },
+  rules: {
+    'prettier/prettier': 0,
+  },
+};
