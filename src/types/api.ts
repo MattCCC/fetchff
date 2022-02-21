654d060f1705ed59ddd45a2a2360cf26f6ed37df
MattCCC <deindesign.pl@gmail.com>
2022-02-21 20:23:47 +0100

    fix: Endpoint types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,11 +1,11 @@
-import { AxiosRequestConfig } from "axios";
+import { EndpointConfig } from "./http-request";
 
 export declare type APIQueryParams = Record<string, any>;
 export declare type APIUrlParams = Record<string, any>;
-export declare type APIRequestConfig = AxiosRequestConfig;
+export declare type APIRequestConfig = EndpointConfig;
 export declare type APIResponse = any;
 
-export declare type Endpoint<T = APIQueryParams, T2 = APIUrlParams, T3 = APIResponse> = (queryParams?: T, urlParams?: T2, requestConfig?: APIRequestConfig) => Promise<T3>;
+export declare type Endpoint<T = APIQueryParams, T2 = APIUrlParams, T3 = APIResponse> = (queryParams?: T, urlParams?: T2, requestConfig?: EndpointConfig) => Promise<T3>;
 
 export interface Endpoints {
     [x: string]: Endpoint;
