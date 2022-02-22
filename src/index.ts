c3e77529561885aec5eee4605675364ee6589d8f
MattCCC <deindesign.pl@gmail.com>
2022-02-22 18:34:12 +0100

    fix: Add API types exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // List of exports
+export * from './types/api';
+export * from './types/http-request';
 export * from './api-handler';
 export * from './http-request-handler';
 export * from './http-request-error-handler';
