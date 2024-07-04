153a30d0c6abfe05c973dd1f9c4c070bd9c2623a
Matt <deindesign.pl@gmail.com>
2024-07-04 02:15:35 +0200

    feat: Improve typings when using classes

--- test/api-handler.spec.ts
+++ test/api-handler.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { ApiHandler } from '../src/api-handler';
 import { mockErrorCallbackClass } from './http-request-error-handler.spec';
